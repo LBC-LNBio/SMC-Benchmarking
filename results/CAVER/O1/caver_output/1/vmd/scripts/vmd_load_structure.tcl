6ac84ccb13973c040c1427183710c68e0645a315
jvsguerra <jvsguerra@gmail.com>
2023-01-18 17:56:30 -0300

    Update CAVER openings results

---
+++ results/CAVER/O1/caver_output/1/vmd/scripts/vmd_load_structure.tcl
@@ -0,0 +1,11 @@
+#set dir "/home/ABTLUS/joao.guerra/CNPEM/gyorgy.szaloki/benchmarking/results/CAVER/O1/caver_output/1/inputs"
+
+mol load pdb ../data/O1.pdb
+
+after idle { 
+  mol representation NewCartoon 
+  mol delrep 0 top
+  mol addrep top
+  mol modcolor 0 top "ColorID" 8
+} 
+
---
+++ results/CAVER/O2/caver_output/1/vmd/scripts/vmd_load_structures.tcl
@@ -0,0 +1,11 @@
+set dir "/home/ABTLUS/joao.guerra/CNPEM/gyorgy.szaloki/benchmarking/results/CAVER/O2/caver_output/1/inputs"
+
+mol load pdb ${dir}/O2.pdb
+
+after idle { 
+  mol representation NewCartoon 
+  mol delrep 0 top
+  mol addrep top
+  mol modcolor 0 top "ColorID" 8
+} 
+
