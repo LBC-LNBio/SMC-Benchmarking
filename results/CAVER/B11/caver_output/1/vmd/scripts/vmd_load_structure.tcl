749a6eb0fa63c010d3823bfbdd0ee1e2cc412441
jvsguerra <jvsguerra@gmail.com>
2023-01-18 18:12:54 -0300

    Update CAVER benchmarking

---
+++ results/CAVER/B11/caver_output/1/vmd/scripts/vmd_load_structure.tcl
@@ -0,0 +1,11 @@
+#set dir "/home/ABTLUS/joao.guerra/CNPEM/gyorgy.szaloki/benchmarking/results/CAVER/B11/caver_output/1/inputs"
+
+mol load pdb ../data/B11.pdb
+
+after idle { 
+  mol representation NewCartoon 
+  mol delrep 0 top
+  mol addrep top
+  mol modcolor 0 top "ColorID" 8
+} 
+
---
+++ results/CAVER/B6/caver_output/1/vmd/scripts/vmd_load_structure.tcl
@@ -0,0 +1,11 @@
+#set dir "/home/ABTLUS/joao.guerra/CNPEM/gyorgy.szaloki/benchmarking/results/CAVER/B6/caver_output/1/inputs"
+
+mol load pdb ../data/B6.pdb
+
+after idle { 
+  mol representation NewCartoon 
+  mol delrep 0 top
+  mol addrep top
+  mol modcolor 0 top "ColorID" 8
+} 
+
